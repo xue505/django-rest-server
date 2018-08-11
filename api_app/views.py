cb36a231e8870e948c06a858b1d28e8f4f70054b
Вадим <vadim@MacBook-Pro-Vadim.local>
2018-08-11 12:52:41 +0200

    + put patch delete methods

--- api_app/views.py
+++ api_app/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
+from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework import status
 from . import serializers
-# Create your views here.
 
 class HelloApiView(APIView):
     """ Test api method """
@@ -11,7 +11,7 @@ class HelloApiView(APIView):
     serializer_class = serializers.HelloSerializer
 
     def get(self, request, format=None):
-        """ GET
+        """ Получение
         return message, an_apiview """
 
         an_apiview = [
@@ -23,7 +23,7 @@ class HelloApiView(APIView):
                          'an_apiview' : an_apiview})
 
     def post(self, request):
-
+        """ Создание """
         serializer = serializers.HelloSerializer(data=request.data)
 
         if serializer.is_valid():
@@ -32,3 +32,27 @@ class HelloApiView(APIView):
             return Response({'message' : message})
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def put(self, request, pk=None):
+        """ Замена """
+        return Response({'method' : 'put'})
+
+    def patch(self, request, pk=None):
+        """ Редактирование """
+        return Response({'method' : 'patch'})
+
+    def delete(self, request, pk=None):
+        """ Удалить """
+        return Response({'method' : 'delete'})
+
+
+class HelloViewSet(viewsets.ViewSet):
+
+    def list(self, request):
+
+        a_viewset = [
+            'bla',
+            'blabla'
+        ]
+
+        return Response({'message' : 'Hello!', 'a_viewset' : a_viewset})
